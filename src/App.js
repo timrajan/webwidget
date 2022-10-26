f1c85d1be826f7436ef07e2075d36ad388738465
Avinash Singh <avidevper09@gmail.com>
2022-10-26 11:58:34 +0530

    fix: conflict styles

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import AtalkiWidget from './components/Widget'
+import React from "react";
+import AtalkiWidget from "./components/Widget";
 
 function App({ domelement, inIframe }) {
-  const id = domelement.getAttribute('data-atalki-docid')
-  const color = domelement.getAttribute('data-atalki-color')
+  const id = domelement.getAttribute("data-atalki-docid");
+  const color = domelement.getAttribute("data-atalki-color");
   // const secColor = domelement.getAttribute('data-secondary-color')
 
   if (!id) {
-    console.error('Doc id is required')
-    return null
+    console.error("Doc id is required");
+    return null;
   }
 
   return (
-    <div className='atalki-main-container'>
-      <AtalkiWidget id={id} color={color || '#027ffb'} inIframe={inIframe} />
+    <div id="atalki-main-container">
+      <AtalkiWidget id={id} color={color || "#027ffb"} inIframe={inIframe} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
